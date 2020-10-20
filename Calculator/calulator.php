cca75dff22102d19c9766bd906699dc5754b1ebd
Katherine <47138336+rocka840@users.noreply.github.com>
2020-10-20 14:14:10 +0200

    Add files via upload

---
+++ Calculator/calulator.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>14th Oct - Calculator</title>
+</head>
+<form action="calc.php" method="get">
+Put a number: <input type="number" name="Num1">
+<br>
+Select: <select value="option">
+        <option value="+">+</option>
+        <option vaule="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+        </select>
+<br>
+Put the second number: <input type="number" name="Num2">
+    <br>
+<input type="button" name="Button" value="go !">
+</form>
+
+<?php
+if(){
+
+}
+?>
+</body>
+</html>
